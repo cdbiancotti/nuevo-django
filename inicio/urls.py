2396bdadcf3e495d03a081cc7f329a45d919782f
Cristian D Biancotti <cdbiancotti@gmail.com>
2023-10-23 22:55:57 -0300

    Playground intermedio II

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from inicio.views import inicio, paletas
 
 urlpatterns = [
-    path('', inicio),
-    path('paletas/', paletas),
+    path('', inicio, name='inicio'),
+    path('paletas/', paletas, name='paletas'),
 ]
