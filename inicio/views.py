ffecb63a9c90700d1955e9027a912761d3f798f1
Cristian D Biancotti <cdbiancotti@gmail.com>
2023-10-25 22:25:25 -0300

    Playground intermedio III

--- inicio/views.py
+++ inicio/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 # from django.template import loader
 from inicio.models import Paleta
+from inicio.forms import CrearPaletaFormulario
 
 def inicio(request):
     
@@ -16,7 +17,49 @@ def inicio(request):
 
 def paletas(request):
     
-    paleta = Paleta(marca='wilson', descripcion='paleta de bela', anio=2022)
-    paleta.save()
+    marca_a_buscar = request.GET.get('marca')
     
-    return render(request, 'inicio/paletas.html', {'paleta': paleta})
+    if marca_a_buscar:
+        listado_de_paletas = Paleta.objects.filter(marca__icontains=marca_a_buscar)
+    else:
+        listado_de_paletas = Paleta.objects.all()
+        
+    return render(request, 'inicio/paletas.html', {'listado_de_paletas': listado_de_paletas})
+
+def crear_paleta(request):
+    
+    # v1 (HTML)
+    # # print('==============')
+    # # print('GET')
+    # # print(request.GET)
+    # # print('==============')
+    # # print('POST')
+    # # print(request.POST)
+    
+    # if request.method == 'POST':
+    #     marca = request.POST.get('marca')
+    #     descripcion = request.POST.get('descripcion')
+    #     anio = request.POST.get('anio')
+        
+    #     paleta = Paleta(marca=marca, descripcion=descripcion, anio=anio)
+    #     paleta.save()
+    
+    # v2 (Django Forms)
+    if request.method == 'POST':
+        formulario = CrearPaletaFormulario(request.POST)
+        if formulario.is_valid():
+            info_limpia = formulario.cleaned_data
+            
+            marca = info_limpia.get('marca')
+            descripcion = info_limpia.get('descripcion')
+            anio = info_limpia.get('anio')
+    
+            paleta = Paleta(marca=marca.lower(), descripcion=descripcion, anio=anio)
+            paleta.save()
+            
+            return redirect('paletas')
+        else:
+            return render(request, 'inicio/crear_paleta.html', {'formulario': formulario})
+        
+    formulario = CrearPaletaFormulario()
+    return render(request, 'inicio/crear_paleta.html', {'formulario': formulario})
