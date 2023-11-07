b86dcbe5c038ed7ca4183a3323b97f101ccee009
Cristian D Biancotti <cdbiancotti@gmail.com>
2023-11-06 22:40:10 -0300

    Django Playground Avanzado III

--- cuentas/forms.py
+++ cuentas/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 class MiFormularioDeCreacion(UserCreationForm):
@@ -11,4 +11,15 @@ class MiFormularioDeCreacion(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         help_texts = {key: '' for key in fields}
-        
+    
+class EdicionPerfil(UserChangeForm):
+    password = None
+    email = forms.EmailField(label='Cambiar email', required=False)
+    first_name = forms.CharField(label='Cambiar nombre', required=False)
+    last_name = forms.CharField(label='Cambiar apellido', required=False)
+    biografia = forms.CharField(max_length=300, required=False, widget=forms.Textarea)
+    avatar = forms.ImageField(required=False)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'biografia', 'avatar']
--- cuentas/views.py
+++ cuentas/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login as django_login
-from cuentas.forms import MiFormularioDeCreacion
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+
+from cuentas.forms import MiFormularioDeCreacion, EdicionPerfil
+from cuentas.models import DatosExtra
 
 
 def login(request):
@@ -17,6 +21,8 @@ def login(request):
             user = authenticate(username=usuario, password=contra)
             
             django_login(request, user)
+            
+            DatosExtra.objects.get_or_create(user=request.user)
             
             return redirect('inicio')
               
@@ -34,3 +40,33 @@ def registro(request):
             return redirect('login')
             
     return render(request, 'cuentas/registro.html', {'formulario_de_registro': formulario})
+
+def editar_perfil(request):
+    
+    datos_extra = request.user.datosextra
+    
+    formulario = EdicionPerfil(instance=request.user, initial={'biografia': datos_extra.biografia, 'avatar': datos_extra.avatar})
+    
+    if request.method == 'POST':
+        formulario = EdicionPerfil(request.POST, request.FILES, instance=request.user)
+        
+        if formulario.is_valid():
+            
+            nueva_biografia = formulario.cleaned_data.get('biografia')
+            nueva_avatar = formulario.cleaned_data.get('avatar')
+            
+            if nueva_biografia:
+                datos_extra.biografia = nueva_biografia
+            if nueva_avatar:
+                datos_extra.avatar = nueva_avatar
+            
+            datos_extra.save()
+            formulario.save()
+            
+            return redirect('editar_perfil')
+    
+    return render(request, 'cuentas/editar_perfil.html', {'formulario': formulario})
+
+class CambiarPassword(PasswordChangeView):
+    template_name = 'cuentas/cambiar_password.html'
+    success_url = reverse_lazy('editar_perfil')
